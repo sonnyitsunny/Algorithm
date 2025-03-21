8c2a7746c66920b4f971c357bb6215c5ccf2e059
SON DONG HYUN <113916318+sonnyitsunny@users.noreply.github.com>
2025-03-21 14:35:34 +0900

    [Silver V] Title: 나이순 정렬, Time: 272 ms, Memory: 55236 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
@@ -1,18 +1,13 @@
-from collections import deque
 import sys
-from bisect import bisect_left,bisect_right
-
-
-sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
-info=[]
-N=int(input())
+
+info = []
+N = int(input())
 for i in range(N):
-    age,name=input().split()
-    info.append((int(i),int(age),name))
+    age, name = input().split()
+    info.append((i, int(age), name))  # 나이를 숫자로 바꿔서 저장
 
-
-info.sort(key=lambda x:(x[1],x[0]))
+info.sort(key=lambda x: (x[1], x[0]))  # 나이 → 가입순 정렬
 
 for person in info:
-    print(person[1],person[2])
+    print(person[1], person[2])
