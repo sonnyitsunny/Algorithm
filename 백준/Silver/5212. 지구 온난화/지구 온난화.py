cbc18bebef252da82612962516fb0130cc4b5a3b
SON DONG HYUN <113916318+sonnyitsunny@users.noreply.github.com>
2025-04-07 21:40:31 +0900

    [Silver II] Title: 지구 온난화, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/5212. 지구 온난화/지구 온난화.py
+++ 백준/Silver/5212. 지구 온난화/지구 온난화.py
@@ -4,7 +4,7 @@ maps=[]
 for _ in range(r):
     maps.append(list(input()))
 
-visited=[ ['.']*c for _ in range(r)]
+new_maps=[ ['.']*c for _ in range(r)]
 
 dx=[-1,1,0,0]
 dy=[0,0,-1,1]
@@ -19,9 +19,9 @@ for i in range(r):
                     cnt+=1
 
             if cnt<3:
-                visited[i][j]='X'
+                new_maps[i][j]='X'
 
-#print(visited)
+
 
 min_row=r
 max_row=0
@@ -31,7 +31,7 @@ max_col=0
 
 for i in range(r):
     for j in range(c):
-        if visited[i][j]=='X':
+        if new_maps[i][j]=='X':
             min_row=min(min_row,i)
             max_row=max(max_row,i)
 
@@ -40,4 +40,4 @@ for i in range(r):
 
 
 for i in range(min_row,max_row+1):
-    print(*visited[i][min_col:max_col+1],sep='')
+    print(*new_maps[i][min_col:max_col+1],sep='')
