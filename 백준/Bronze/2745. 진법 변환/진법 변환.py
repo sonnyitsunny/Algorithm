bb3ecf2443a3d69d7b446448b3d4c615c8ce529a
SON DONG HYUN <113916318+sonnyitsunny@users.noreply.github.com>
2025-05-05 22:08:48 +0900

    [Bronze II] Title: 진법 변환, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2745. 진법 변환/진법 변환.py
@@ -0,0 +1,43 @@
+hash={
+    'A':10,
+    'B':11,
+    'C':12,
+    'D':13,
+    'E':14,
+    'F':15,
+    'G':16,
+    'H':17,
+    'I':18,
+    'J':19,
+    'K':20,
+    'L':21,
+    'M':22,
+    'N':23,
+    'O':24,
+    'P':25,
+    'Q':26,
+    'R':27,
+    'S':28,
+    'T':29,
+    'U':30,
+    'V':31,
+    'W':32,
+    'X':33,
+    'Y':34,
+    'Z':35
+}
+N,B=input().split()
+B=int(B)
+length=len(N)
+result=0
+total=0
+
+for i in range(length):
+    num=N[i]
+    if num.isdigit():
+        total=int(num)*B**(length-1-i)
+        result+=total
+    else:
+        total=hash[num]*B**(length-1-i)
+        result+=total
+print(result)
