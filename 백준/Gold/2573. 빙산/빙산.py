c90650b868dea654f1077d6ce12d54ecfc21f8f3
SON DONG HYUN <113916318+sonnyitsunny@users.noreply.github.com>
2025-05-18 23:47:00 +0900

    [Gold IV] Title: 빙산, Time: 3020 ms, Memory: 35776 KB -BaekjoonHub

--- 백준/Gold/2573. 빙산/빙산.py
+++ 백준/Gold/2573. 빙산/빙산.py
@@ -1,77 +1,61 @@
 from collections import deque
 import sys
-#input = sys.stdin.readline
-#while문 안에 이중포문
-#녹이기 실행 그 녹인 거는 다른 맵에 반영
-#녹이기 1년 했으면 year+1하고 bfs로 덩어리 갯수 파악 그게 2이상 이면 year 출력
-import copy
+input = sys.stdin.readline
+
 n, m = map(int, input().split())
-graph=[list(map(int,input().split())) for _ in range(n)]
-ice=[[0]*m for _ in range(n)]
-year=0
+graph = [list(map(int, input().split())) for _ in range(n)]
 
-dx=[-1,1,0,0]
-dy=[0,0,-1,1]
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 
-def melt(x,y):
-    zero_cnt=0
-    for k in range(4):
-        nx,ny=x+dx[k],y+dy[k]
-        if 0<=nx<n and 0<=ny<m:
-            if graph[nx][ny]==0:
-                zero_cnt+=1
-    
-    height=graph[x][y]-zero_cnt
-    if height<=0:
-        ice[x][y]=0
-    else:
-        ice[x][y]=height
-
-def bfs(x,y):
-    visited[x][y]=True
-    q=deque()
-    q.append((x,y))
-    while q:
-        x,y=q.popleft()
+def melt(icebergs):
+    # 현재 빙산 위치만 녹임
+    new_graph = [row[:] for row in graph]
+    for x, y in icebergs:
+        sea = 0
         for k in range(4):
-            nx,ny=x+dx[k],y+dy[k]
-            if 0<=nx<n and 0<=ny<m:
-                if not visited[nx][ny] and graph[nx][ny]!=0:
-                    visited[nx][ny]=True
-                    q.append((nx,ny))
+            nx, ny = x + dx[k], y + dy[k]
+            if 0 <= nx < n and 0 <= ny < m:
+                if graph[nx][ny] == 0:
+                    sea += 1
+        new_graph[x][y] = max(0, graph[x][y] - sea)
+    return new_graph
 
+def bfs(x, y, visited):
+    q = deque()
+    q.append((x, y))
+    visited[x][y] = True
+    while q:
+        x, y = q.popleft()
+        for k in range(4):
+            nx, ny = x + dx[k], y + dy[k]
+            if 0 <= nx < n and 0 <= ny < m:
+                if graph[nx][ny] > 0 and not visited[nx][ny]:
+                    visited[nx][ny] = True
+                    q.append((nx, ny))
 
-safe=False
+year = 0
 while True:
-    land=0
-    for i in range(n):
-        for j in range(m):
-            if graph[i][j]!=0:
-                melt(i,j)
-    year+=1
-    graph=[]
-    for row in ice:
-        graph.append(row)
+    # 빙산 좌표 수집
+    icebergs = [(i, j) for i in range(n) for j in range(m) if graph[i][j] > 0]
 
-    ice=[[0]*m for _ in range(n)]
-
-    visited=[[False]*m for _ in range(n)]
-    for i in range(n):
-        for j in range(m):
-            if not visited[i][j] and graph[i][j]>0:
-                bfs(i,j)
-                land+=1
-
-    if land>=2:
-        #print(land)
-        print(year)
-        #for row in graph:
-            #print(row)
-        break
-
-    if land==0:
+    # 빙산이 아예 없으면 종료
+    if not icebergs:
         print(0)
         break
 
+    # 덩어리 개수 확인
+    visited = [[False] * m for _ in range(n)]
+    land = 0
+    for x, y in icebergs:
+        if not visited[x][y]:
+            bfs(x, y, visited)
+            land += 1
 
+    if land >= 2:
+        print(year)
+        break
 
+    # 1년 경과 및 녹이기
+    graph = melt(icebergs)
+    year += 1
