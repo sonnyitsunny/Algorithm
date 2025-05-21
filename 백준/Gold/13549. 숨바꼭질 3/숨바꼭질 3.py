e6658c7e756e08c3c8c5a5a238597fb3c800d5a1
SON DONG HYUN <113916318+sonnyitsunny@users.noreply.github.com>
2025-05-21 23:19:13 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 144 ms, Memory: 36376 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
@@ -1,31 +1,42 @@
+import sys
 from collections import deque
+input=sys.stdin.readline
 
-n, k = map(int, input().split())
-MAX = 100001
-dist = [-1] * MAX
+# 위치 확인 
 
-q = deque()
-q.append(n)
-dist[n] = 0
+
+n,k=map(int,input().split())
+visited=[False]*(100001)
+
+q=deque()
+q.append((n,0)) # 수빈이 위치랑 시간 넣기
+visited[n]=True
 
 while q:
-    cur = q.popleft()
-
-    if cur == k:
-        print(dist[cur])
+    subin,time=q.popleft()
+    if subin==k:
+        print(time)
         break
 
-    # 순간이동 (0초)
-    if 0 <= cur * 2 < MAX and dist[cur * 2] == -1:
-        dist[cur * 2] = dist[cur]
-        q.appendleft(cur * 2)
+    for move in (subin*2,subin-1,subin+1):
+        if 0<=move<=100000 and not visited[move]:
+            if move==subin*2:
+                
+                
+                visited[move]=True
+                q.append((move,time))
+                
+                #print(1)
+            elif move==subin+1:
+                
+             
+                visited[move]=True
+                q.append((move,time+1))
+                #print(2)
+            elif move==subin-1:
+                
+                visited[move]=True
+                q.append((move,time+1))
+                #print(3)
 
-    # 뒤로 걷기 (1초)
-    if 0 <= cur - 1 < MAX and dist[cur - 1] == -1:
-        dist[cur - 1] = dist[cur] + 1
-        q.append(cur - 1)
 
-    # 앞으로 걷기 (1초)
-    if 0 <= cur + 1 < MAX and dist[cur + 1] == -1:
-        dist[cur + 1] = dist[cur] + 1
-        q.append(cur + 1)
