6ab87931e382e49829d7442b779fa864abfef2d1
SON DONG HYUN <113916318+sonnyitsunny@users.noreply.github.com>
2025-05-22 01:13:59 +0900

    [Gold V] Title: 회의실 배정, Time: 252 ms, Memory: 54920 KB -BaekjoonHub

--- 백준/Gold/1931. 회의실 배정/회의실 배정.py
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.py
@@ -10,12 +10,12 @@ for _ in range(n):
 
 arr.sort(key=lambda x: (x[1],x[0]))
 cnt=0
-start=0
+before_end_time=0
 for x,y in arr:
     #직전 종료 시간이 지금 시작 시간보다 작다면
-    if start<=x:
+    if before_end_time<=x:
         cnt+=1
-        start=y
+        before_end_time=y
     else:
         continue
 
