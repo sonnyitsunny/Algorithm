9fd8acfa5031ba209f3ff28540ffe8f655db03d3
SON DONG HYUN <113916318+sonnyitsunny@users.noreply.github.com>
2025-09-21 15:16:41 +0900

    [Gold V] Title: 수 고르기, Time: 164 ms, Memory: 36264 KB -BaekjoonHub

---
+++ 백준/Gold/2230. 수 고르기/수 고르기.py
@@ -0,0 +1,25 @@
+import sys
+
+
+input = sys.stdin.readline
+
+N,M=map(int,input().split())
+arr=[]
+for _ in range(N):
+    arr.append(int(input()))
+
+ans=float('inf')
+arr.sort()
+
+left=0
+right=0
+
+while left < N and right<N:
+    diff=arr[right]-arr[left]
+    if diff<M:
+        right+=1
+    else:
+        ans=min(ans,diff)
+        left+=1
+        
+print(ans)
