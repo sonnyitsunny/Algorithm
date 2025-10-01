b31d078400896096c009495513a6b9dc5ab81398
SON DONG HYUN <113916318+sonnyitsunny@users.noreply.github.com>
2025-10-01 23:27:17 +0900

    [Silver V] Title: 날짜 계산, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1476. 날짜 계산/날짜 계산.py
@@ -0,0 +1,31 @@
+import sys
+input = sys.stdin.readline
+
+E,S,M=map(int,input().split())
+
+year=1
+e=1
+s=1
+m=1
+
+
+while True:
+    if e==E and s==S and m==M:
+        print(year)
+        break
+    
+    year+=1
+    if e<15:
+        e+=1
+    else:
+        e=1
+
+    if s<28:
+        s+=1
+    else:
+        s=1
+    
+    if m<19:
+        m+=1
+    else:
+        m=1
