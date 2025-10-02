a24b8aa52d0c27ba4843dc3cca32c89d0f408a87
SON DONG HYUN <113916318+sonnyitsunny@users.noreply.github.com>
2025-10-02 17:35:32 +0900

    [Silver I] Title: 오르막 수, Time: 44 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Silver/11057. 오르막 수/오르막 수.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+N=int(input())
+
+dp = [[0] * 10 for _ in range(N+1)]
+
+for i in range(10):
+    dp[1][i]=1
+
+
+for i in range(2,N+1):
+    for j in range(10):
+        if j==10:
+            dp[i][j]==dp[i-1][j]
+
+        else:
+            dp[i][j]=(dp[i][j-1]+dp[i-1][j])
+
+print(sum(dp[N]) % 10007)
