03f7cf88df55766eb5fffb47a96f41b8398757ba
SON DONG HYUN <113916318+sonnyitsunny@users.noreply.github.com>
2025-11-17 20:51:45 +0900

    [Gold IV] Title: 스도쿠, Time: 2340 ms, Memory: 132592 KB -BaekjoonHub

--- 백준/Gold/2580. 스도쿠/스도쿠.py
+++ 백준/Gold/2580. 스도쿠/스도쿠.py
@@ -1,7 +1,5 @@
 import sys
 input = sys.stdin.readline
-sys.setrecursionlimit(10**8)
-
 
 board=[]
 for _ in range(9):
@@ -10,19 +8,34 @@ for _ in range(9):
 empty=[]
 
 
-row = [[False]*10 for _ in range(9)]
-col = [[False]*10 for _ in range(9)]
-box = [[False]*10 for _ in range(9)]
+
 for i in range(9):
     for j in range(9):
         num = board[i][j]
         if num == 0:
             empty.append((i, j))
-        else:
-            row[i][num] = True
-            col[j][num] = True
-            box[(i//3)*3 + (j//3)][num] = True  # 3×3 박스 인덱스
 
+
+def possible(x, y, n):
+    # 행 체크
+    for i in range(9):
+        if board[x][i] == n:
+            return False
+
+    # 열 체크
+    for i in range(9):
+        if board[i][y] == n:
+            return False
+
+    # 3x3 박스 체크
+    bx = (x // 3) * 3
+    by = (y // 3) * 3
+    for i in range(3):
+        for j in range(3):
+            if board[bx + i][by + j] == n:
+                return False
+
+    return True
 
 def dfs(idx):
     if idx==len(empty):
@@ -31,20 +44,13 @@ def dfs(idx):
         sys.exit(0)
         
 
-
-
     x,y=empty[idx]
-    b = (x//3)*3 + (y//3)
-    for num in range(1,10):
-        if not row[x][num] and not col[y][num] and not box[b][num]:
-            board[x][y] = num
-            row[x][num] = col[y][num] = box[b][num] = True
-
-            dfs(idx+1)
-
-            # 백트래킹 되돌리기
-            board[x][y] = 0
-            row[x][num] = col[y][num] = box[b][num] = False
+   
+    for num in range(1, 10):
+            if possible(x, y, num):
+                board[x][y] = num
+                dfs(idx + 1)
+                board[x][y] = 0  
 
 
 
