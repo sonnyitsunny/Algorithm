0151556041210444eee5eea89dcb9b7b7af5f235
SON DONG HYUN <113916318+sonnyitsunny@users.noreply.github.com>
2025-11-18 21:33:23 +0900

    [Silver II] Title: 차이를 최대로, Time: 124 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/10819. 차이를 최대로/차이를 최대로.py
+++ 백준/Silver/10819. 차이를 최대로/차이를 최대로.py
@@ -1,15 +1,41 @@
 import sys
 input = sys.stdin.readline
-from itertools import permutations
+
 n=int(input())
 arr=list(map(int,input().split()))
 
 max_cnt=0
-for per in permutations(arr):
-    cnt=0
-    for i in range(len(per)-1):
-        cnt+=(abs(per[i]-per[i+1]))
 
-    max_cnt=max(max_cnt,cnt)
+visited=[False]*n
+temp=[]
 
+def dfs():
+
+    global max_cnt
+    if len(temp)==n:
+        cnt=0
+        for i in range(n-1):
+            cnt+=abs(temp[i]-temp[i-1])
+
+        max_cnt=max(cnt,max_cnt)
+        return
+    
+
+    for i in range(n):
+        if not visited[i]:
+            temp.append(arr[i])
+            visited[i]=True
+            dfs()
+
+            temp.pop()
+            visited[i]=False
+
+
+
+
+
+
+
+
+dfs()
 print(max_cnt)
