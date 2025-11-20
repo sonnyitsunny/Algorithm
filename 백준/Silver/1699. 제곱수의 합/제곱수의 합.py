5755d6a552724755c5545d5f9496f2ecc78f58a4
SON DONG HYUN <113916318+sonnyitsunny@users.noreply.github.com>
2025-11-20 14:25:54 +0900

    [Silver II] Title: 제곱수의 합, Time: 164 ms, Memory: 110328 KB -BaekjoonHub

---
+++ 백준/Silver/1699. 제곱수의 합/제곱수의 합.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+n=int(input())
+
+
+dp=[0]*(n+1)
+dp[1]=1
+
+for i in range(2,n+1):
+    
+    dp[i]=i
+    j=1
+    while j*j<=i:
+        dp[i]=min(dp[i],dp[i-j*j]+1)
+        j+=1
+
+print(dp[n])
