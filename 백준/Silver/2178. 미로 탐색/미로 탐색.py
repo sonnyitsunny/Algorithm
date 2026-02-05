c84e02e1d0bb46144b697d0f9eb15688e751436f
sonnyitsunny <113916318+sonnyitsunny@users.noreply.github.com>
2026-02-06 00:04:22 +0900

    [Silver I] Title: 미로 탐색, Time: 60 ms, Memory: 34952 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,34 +1,34 @@
-import sys
-input=sys.stdin.readline
+
+# 1은 이동할 수 있는 칸을 나타내고, 0은 이동할 수 없는 칸
+#(1,1) -> (N,M) - 1 based index
+
 from collections import deque
 
-def bfs(x,y):
-    dx=[0,1,-1,0]
-    dy=[1,0,0,-1]
+N,M=map(int,input().split()) 
+board=[]
+for _ in range(N):
+    board.append(list(map(int,input())))
+
+#상하좌우
+dx=[-1,1,0,0]
+dy=[0,0,-1,1]
+
+visited=[[-1]*M for _ in range(N)]
+
+q=deque()
+visited[0][0]=1
+q.append((0,0))
+
+while q:
+    x,y = q.popleft()
+
+    if x==N-1 and y==M-1:
+        print(visited[N-1][M-1])
+        break
     
-    queue=deque()
-    queue.append((x,y))
-
-    while queue:
-        x,y=queue.popleft()
-        for i in range(0,4):
-            nx=x+dx[i]
-            ny=y+dy[i]
-            
-            if nx>=0 and nx<N and ny>=0 and ny<M:
-                if matrix[nx][ny]!=0 and matrix[nx][ny]==1:
-                    matrix[nx][ny]=matrix[x][y]+1
-                    queue.append((nx,ny))
-                else:
-                    continue
-            else:
-                continue
-    return matrix[N-1][M-1]
-
-N,M=map(int,input().split())
-
-matrix=[]
-for _ in range(0,N):
-    matrix.append(list(map(int,input().strip())))
-
-print(bfs(0,0))
+    for k in range(4):
+        nx,ny=x+dx[k],y+dy[k]
+        if 0<=nx<N and 0<=ny<M:
+            if visited[nx][ny]==-1 and board[nx][ny]==1:
+                visited[nx][ny]=visited[x][y]+1
+                q.append((nx,ny))
