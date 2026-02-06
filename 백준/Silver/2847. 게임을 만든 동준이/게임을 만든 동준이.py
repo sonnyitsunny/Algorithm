86a616b8c6dd0b359624bbdbe7b4a48e23475069
sonnyitsunny <113916318+sonnyitsunny@users.noreply.github.com>
2026-02-06 15:26:25 +0900

    [Silver IV] Title: 게임을 만든 동준이, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2847. 게임을 만든 동준이/게임을 만든 동준이.py
+++ 백준/Silver/2847. 게임을 만든 동준이/게임을 만든 동준이.py
@@ -1,22 +1,18 @@
-import sys
-input = sys.stdin.readline
-
-
+#배열로 받고 reverse해서 감소하는 배열로 
 n=int(input())
+
 arr=[]
+cnt=0
+
 for _ in range(n):
     arr.append(int(input()))
+arr.reverse()
 
-
-ans=0
-
-for i in range(n-1,0,-1):
-    
-    if arr[i]>arr[i-1]:
+for i in range(n-1):
+    if arr[i]>arr[i+1]:
         continue
+    diff=arr[i+1]-arr[i]
+    arr[i+1]=arr[i]-1
+    cnt+=diff+1
 
-    else:
-        plus=arr[i-1]-arr[i]+1
-        arr[i-1]=arr[i-1]-plus
-        ans+=plus
-print(ans)
+print(cnt)
