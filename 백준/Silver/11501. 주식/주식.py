2923e286f58b5ea55466764c19b9a353e762db42
sonnyitsunny <113916318+sonnyitsunny@users.noreply.github.com>
2026-02-24 22:39:39 +0900

    [Silver II] Title: 주식, Time: 3184 ms, Memory: 153720 KB -BaekjoonHub

--- 백준/Silver/11501. 주식/주식.py
+++ 백준/Silver/11501. 주식/주식.py
@@ -1,19 +1,13 @@
-import sys
-input = sys.stdin.readline
-
 T=int(input())
-
 for _ in range(T):
     N=int(input())
-    days=list(map(int,input().split()))
+    price=list(map(int,input().split()))
+
     money=0
-
-    best=days[-1]
-
+    top_price=price[-1]
     for i in range(N-2,-1,-1):
-        if best>days[i]:
-            money+=best-days[i]
+        if top_price>price[i]:
+            money+=top_price-price[i]
         else:
-            best=days[i]
-       #print(best,days[i])
+            top_price=price[i]
     print(money)
