86dd6110c0dd6e72883dcaa8c90f292f3ba735aa
sonnyitsunny <113916318+sonnyitsunny@users.noreply.github.com>
2026-02-24 23:10:50 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 6348 ms, Memory: 191708 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
@@ -1,47 +1,41 @@
 from collections import deque
-import sys
-#input = sys.stdin.readline
 
-
-n, m = map(int, input().split())
+N,M=map(int,input().split())
 graph=[]
-
-for _ in range(n):
+for _ in range(N):
     graph.append(list(map(int,input())))
 
-#broke[][][1] 이 부순거
-broke=[[[0]*2 for _ in range(m)] for _ in range(n)]
+
+board=[[[0]*2 for _ in range(M)] for _ in range(N)] #[][][1] 이 벽뚫은거임
+
 
 q=deque()
+board[0][0][0]=1
 q.append((0,0,0))
-broke[0][0][0]=1
+
 dx=[-1,1,0,0]
 dy=[0,0,-1,1]
+
 safe=False
+
 while q:
     x,y,br=q.popleft()
 
-
-    if x==n-1 and y==m-1:
-        print(broke[x][y][br])
-        safe=True
-        break
-
-
+    if x==N-1 and y==M-1:
+        print(board[N-1][M-1][br])
+        exit()
+        
 
     for k in range(4):
-        nx=x+dx[k]
-        ny=y+dy[k]
-
-        if 0<=nx<n and 0<=ny<m:
-
-            #벽 아니고, 안가본 곳
-            if graph[nx][ny]==0 and broke[nx][ny][br]==0:
-                broke[nx][ny][br]=broke[x][y][br]+1
+        nx,ny=x+dx[k],y+dy[k]
+        if 0<=nx<N and 0<=ny<M:
+            #벽없는 경우
+            if graph[nx][ny]==0 and board[nx][ny][br]==0:
+                board[nx][ny][br]=board[x][y][br]+1
                 q.append((nx,ny,br))
-            # 벽이고 아직 안부쉈으면 
-            elif graph[nx][ny] == 1 and br==0 and broke[nx][ny][1]==0:
-                broke[nx][ny][1]=broke[x][y][0]+1
-                q.append((nx, ny, 1))
-if not safe:
-    print(-1)
+
+            elif graph[nx][ny]==1 and br==0 and board[nx][ny][1]==0:
+                board[nx][ny][1]=board[x][y][0]+1
+                q.append((nx,ny,1))
+
+print(-1)
