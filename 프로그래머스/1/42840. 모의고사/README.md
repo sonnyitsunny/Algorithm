c5cefa5780d767ce6b5aefbf3863a00ff14a57eb
sonnyitsunny <113916318+sonnyitsunny@users.noreply.github.com>
2026-09-09 21:39:44 +0900

    [level 1] Title: 모의고사, Time: 1.12 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/README.md
+++ 프로그래머스/1/42840. 모의고사/README.md
@@ -1,10 +1,10 @@
 # [level 1] 모의고사 - 42840 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840?language=python3) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 4.93 ms
+메모리: 11.6 MB, 시간: 1.12 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 06일 20:25:57
+2026년 09월 09일 21:39:40
 
 ### 문제 설명
 
--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,31 +1,29 @@
 def solution(answers):
     answer = []
-    #방식세팅 후 answer 반복문 돌리기하고 방식이랑 비교 % 연산자 이용
-    #같으면 cnt1, cnt2, cnt3 추가
+    p1=[1, 2, 3, 4, 5]
+    p2=[2, 1, 2, 3, 2, 4, 2, 5]
+    p3=[3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
     
-    dic={'cnt1':0,'cnt2':0,'cnt3':0}
-    
-    p1=[1,2,3,4,5] # %5
-    p2=[2, 1, 2, 3, 2, 4, 2, 5] # %8
-    p3=[3, 3, 1, 1, 2, 2, 4, 4, 5, 5] # %10
-    
-    for i in range(0,len(answers)):
-        if p1[i%5]==answers[i]:
-            dic['cnt1']+=1
-        if p2[i%8]==answers[i]:
-            dic['cnt2']+=1
-        if p3[i%10]==answers[i]:
-            dic['cnt3']+=1
-    max_key=max(dic['cnt1'],dic['cnt2'],dic['cnt3'])
-    
-    result=[k for k,v in dic.items() if v==max_key]
-    for i in result:
-        if i=='cnt1':
-            answer.append(1)
-        if i=='cnt2':
-            answer.append(2)
-        if i=='cnt3':
-            answer.append(3)
-            
+    ps1=0
+    ps2=0
+    ps3=0
+    n=len(answers)
+    max_r=0
+    for i in range(n):
+        if answers[i]==p1[i%len(p1)]:
+            ps1+=1
+        if answers[i]==p2[i%len(p2)]:
+            ps2+=1     
+        if answers[i]==p3[i%len(p3)]:
+            ps3+=1
+    max_r=max(ps1,ps2,ps3)
+    if max_r==0:
+        return answer
+    if ps1==max_r:
+        answer.append(1)
+    if ps2==max_r:
+        answer.append(2)   
+    if ps3==max_r:
+        answer.append(3)
     answer.sort()
     return answer
