dc76fd48e5d40f4e59772514e6002edae5e0d0c5
sonnyitsunny <113916318+sonnyitsunny@users.noreply.github.com>
2026-09-22 21:26:06 +0900

    [level 2] Title: 구명보트, Time: 7.49 ms, Memory: 11.9 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.py
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -1,34 +1,20 @@
-# from bisect import bisect_right
 def solution(people, limit):
-    answer=0
-    i,j=0,len(people)-1
+    answer = 0
+    n=len(people)
     people.sort()
-    while i<=j:
-        if people[i]+people[j]<=limit:
-            i+=1
-        j-=1
-        answer+=1
-            
-                    
-    return answer
-
-
-# answer = 0
-#     n=len(people)
-#     people.sort()
-#     visited=[False]*(n)
+    left=0
+    right=n-1
     
-#     for i in range(n-1,-1,-1):
-#         if not visited[i]:
-#             weight=limit-people[i]
-#             index=bisect_right(people,weight)
-#             visited[i]=True
-#             alone=True
-#             for j in range(index-1,-1,-1):
-#                 if not visited[j]:
-#                     visited[j]=True
-#                     answer+=1
-#                     alone=False
-#                     break
-#             if alone:
-#                 answer+=1
+    while left<=right:
+        #tmp는 남은 kg
+        tmp=limit-people[right]
+        
+        if people[left]<=tmp:
+            answer+=1
+            left+=1
+            right-=1
+        else:
+            answer+=1
+            right-=1
+    
+    return answer
